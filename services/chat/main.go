01af05cde1329c7b87e6db2db6781ee471d7e0aa
Ivan Netskin <ivannetskin@gmail.com>
2024-08-01 15:07:12 +0300

    Add docker compose support

---
+++ services/chat/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// поход в сервис app
+func RequestExternalService() int {
+	resp, err := http.Get("app:http/hello")
+	if err != nil {
+		log.Println(err)
+		return http.StatusInternalServerError
+	}
+
+	return resp.StatusCode
+}
+
+func main() {
+	e := echo.New()
+
+	fmt.Println("chat service started")
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(RequestExternalService(), "Hello, Docker! <3")
+	})
+
+	e.GET("/isReady", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	e.GET("/isAlive", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	e.GET("/isStarted", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
